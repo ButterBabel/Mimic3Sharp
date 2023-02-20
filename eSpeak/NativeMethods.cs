0407b13ccda4c5a1669f603ea167982b3ddc9f34
J. Zebedee <zebedee@code.gripe>
2023-02-20 02:52:30 -0600

    espeak test

---
+++ eSpeak/NativeMethods.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Mimic3Sharp.eSpeak;
+
+internal static partial class NativeMethods
+{
+    private const string LibraryName = "libespeak-ng";
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial Error espeak_SetVoiceByName(string name);
+
+    [LibraryImport(LibraryName)]
+    public static partial Error espeak_SetParameter(Parameter parameter, int value, ParameterType type);
+
+    [LibraryImport(LibraryName)]
+    public static partial IntPtr espeak_GetCurrentVoice();
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial Error espeak_Synth(string text, int size, uint startPosition = 0, PositionType positionType = PositionType.Character, uint endPosition = 0, SpeechFlags flags = SpeechFlags.CharsUtf8, UIntPtr uniqueIdentifier = default(UIntPtr), IntPtr userData = default(IntPtr));
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial Error espeak_Initialize(AudioOutput output, int bufferLength, string path, int options);
+
+    [LibraryImport(LibraryName)]
+    public static partial Error espeak_Cancel();
+
+    [LibraryImport(LibraryName)]
+    public static partial void espeak_SetSynthCallback(SynthCallback callback);
+}
+
+public delegate int SynthCallback(IntPtr wavePtr, int bufferLength, IntPtr eventsPtr);
+
+enum Parameter
+{
+    Rate = 1,
+    Volume = 2,
+    Pitch = 3,
+    Range = 4,
+    Punctuation = 5,
+    Capitals = 6,
+    WordGap = 7,
+    Intonation = 9,
+}
+
+enum Error
+{
+    EE_OK = 0,
+    EE_INTERNAL_ERROR = -1,
+    EE_BUFFER_FULL = 1,
+    EE_NOT_FOUND = 2
+}
+
+enum ParameterType
+{
+    Absolute = 0,
+    Relative = 1
+}
+
+enum AudioOutput
+{
+    Playback,
+    Retrieval,
+    Synchronous,
+    SynchronousPlayback
+};
+enum PositionType
+{
+    Character = 1,
+    Word = 2,
+    Sentence = 3
+}
+[Flags]
+enum SpeechFlags
+{
+    CharsUtf8 = 1,
+    SSML = 0x10,
+}
---
+++ eSpeak/eSpeakVoice.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Tensorflow;
+
+namespace Mimic3Sharp.eSpeak;
+using static NativeMethods;
+
+internal static class eSpeakVoice
+{
+    public static void Initialize(string path)
+    {
+        var khz = espeak_Initialize(AudioOutput.Retrieval, 0, path, 0) switch
+        {
+            Error.EE_INTERNAL_ERROR => throw new Exception($"Could not initialize eSpeak. Maybe there is no espeak data at {path}?"),
+            Error e => (int)e
+        };
+
+        espeak_SetSynthCallback(Handle);
+
+        //Initialized = true;
+    }
+
+    static bool CheckResult(Error result)
+    {
+        if (result == Error.EE_OK)
+        {
+            return true;
+        }
+        else if (result == Error.EE_BUFFER_FULL)
+        {
+            return false;
+        }
+        else if (result == Error.EE_INTERNAL_ERROR)
+        {
+            throw new Exception("Internal error in ESpeak.");
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool Speak(string text)
+    {
+        var result = espeak_Synth(text, text.Length);
+        return CheckResult(result);
+    }
+
+    public static int Handle(IntPtr wavePtr, int bufferLength, IntPtr eventsPtr)
+    {
+        Console.WriteLine("Received event!");
+        Console.WriteLine("Buffer length is " + bufferLength);
+
+        //if (bufferLength == 0)
+        //{
+        //    /*
+        //    var file = new FileStream("alarm01.wav", FileMode.Open);
+        //    Stream.Seek(0, SeekOrigin.Begin);
+        //    file.CopyTo(Stream);
+        //    */
+        //    PlayAudio();
+        //    Console.Write(ConvertHeadersToString(Stream.GetBuffer()));
+        //    Stream.Dispose();
+        //    return 0;
+        //}
+
+        //WriteAudioToStream(wavePtr, bufferLength);
+
+        //var events = MarshalEvents(eventsPtr);
+
+        //foreach (Event anEvent in events)
+        //{
+        //    Console.WriteLine(anEvent.Type);
+        //    Console.WriteLine(anEvent.Id);
+        //}
+
+        return 0; // continue synthesis
+    }
+}
