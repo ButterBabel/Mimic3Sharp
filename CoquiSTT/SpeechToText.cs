0340aa46f7b990ba770d99ba03642117ce659fbc
J. Zebedee <zebedee@code.gripe>
2023-03-18 10:40:22 -0500

    Add CoquiSTT demo and assets

---
+++ CoquiSTT/SpeechToText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimic3Sharp.CoquiSTT;
+using static NativeMethods;
+internal class SpeechToText {
+    private static void ThrowIfNotOk([DoesNotReturnIf(true)] ErrorCodes result) {
+        if (result is not ErrorCodes.STT_ERR_OK) {
+            throw new InvalidOperationException(STT_ErrorCodeToErrorMessage(result));
+        }
+    }
+
+    public unsafe SpeechToText(string modelPath) {
+        ArgumentNullException.ThrowIfNullOrEmpty(modelPath);
+
+        var result = STT_CreateModel(modelPath, out var modelState);
+        ThrowIfNotOk(result);
+
+        ;
+    }
+}
